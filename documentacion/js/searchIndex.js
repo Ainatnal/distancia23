75a3ff67956e597477afd16885e4a70887d5ef5f
Ainatnal <132765122+Ainatnal@users.noreply.github.com>
2023-05-06 16:59:50 +0200

    Add files via upload

---
+++ documentacion/js/searchIndex.js
@@ -0,0 +1,39 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\sumar\u0028\u0029",
+            "name": "sumar",
+            "summary": "Funci\u00F3n\u0020para\u0020sumar\u0020enteros",
+            "url": "namespaces/default.html#function_sumar"
+        },                {
+            "fqsen": "\\multiplicar\u0028\u0029",
+            "name": "multiplicar",
+            "summary": "Funci\u00F3n\u0020para\u0020multiplicar\u0020entetros\u0020diferentes\u0020de\u00200",
+            "url": "namespaces/default.html#function_multiplicar"
+        },                {
+            "fqsen": "\\ListaTareas",
+            "name": "ListaTareas",
+            "summary": "Clase\u0020para\u0020manejar\u0020una\u0020lista\u0020de\u0020tareas.",
+            "url": "classes/ListaTareas.html"
+        },                {
+            "fqsen": "\\ListaTareas\u003A\u003AagregarTarea\u0028\u0029",
+            "name": "agregarTarea",
+            "summary": "Agrega\u0020una\u0020tarea\u0020a\u0020la\u0020lista",
+            "url": "classes/ListaTareas.html#method_agregarTarea"
+        },                {
+            "fqsen": "\\ListaTareas\u003A\u003AobtenerTareas\u0028\u0029",
+            "name": "obtenerTareas",
+            "summary": "Obtiene\u0020la\u0020lista\u0020de\u0020tareas\u0020completa",
+            "url": "classes/ListaTareas.html#method_obtenerTareas"
+        },                {
+            "fqsen": "\\ListaTareas\u003A\u003A\u0024tareas",
+            "name": "tareas",
+            "summary": "",
+            "url": "classes/ListaTareas.html#property_tareas"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
